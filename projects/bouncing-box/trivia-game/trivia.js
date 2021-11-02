b0d1f7cb84889177e0e6a98e2ba7fba1893efa4b
molsen2 <meo9165@ppsbstudent.org>
2021-11-02 19:27:01 +0000

    Trivia Game

---
+++ projects/bouncing-box/trivia-game/trivia.js
@@ -0,0 +1,26 @@
+var categories = ["Science", "World History", "Arts and Culture", "Before and After", "Hard Dictionary Words", "Academy Award Winning Movies", "70's Hits", "5 Letter Words", "Holidays"];
+
+var scienceQs = [
+    "In a Glossectomy, which body part is removed?",
+    "Who was the founder of the cause of Beri Beri?",
+    "What is the sudy of kidneys called?"
+];
+var worldHistoryQs = [
+    "The Taj Mahal was constructed as a tomb of the wife of which Mughal Emperor?"
+];
+var artsAndCulturesQs = [];
+
+var scienceAs = [
+    "Tongue",
+    "Eijkman",
+    "Nephrology"
+];
+var worldHistoryAs = [
+    "Shah Jahan"
+];
+var artsAndCulturesQs = [];
+
+var userAnswersSci = [];
+var userAnswersWH = [];
+var userAnswersAAC = [];
+
