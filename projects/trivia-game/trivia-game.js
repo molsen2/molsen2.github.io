c57dbabba3971e8627e89af54c07449198702efc
molsen2 <meo9165@ppsbstudent.org>
2021-11-22 19:22:12 +0000

    trivia game

---
+++ projects/trivia-game/trivia-game.js
@@ -0,0 +1,10 @@
+var scienceQuestions = [];
+var seventiesHits = [];
+var fiveLetterWords = [];
+var computerScience = [];
+var randomHolidays = [];
+var literature = [];
+
+for(times = 0; times <= 1; times++){
+    
+}
