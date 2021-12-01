b5e0aa5716657bb2d283d64e28d20e35884bea24
molsen2 <meo9165@ppsbstudent.org>
2021-12-01 04:26:11 +0000

    trivia game

--- projects/trivia-game/triviagame.js
+++ projects/trivia-game/triviagame.js
@@ -3,7 +3,7 @@ var seventiesHits = [];
 var randomHolidays = [];
 var literature = [];
 
-for(times = 0; times <= 1; times++){
+/*for(times = 0; times <= 1; times++){
     var randomNum = assignNum();
     if(randomNum === 0){
         
@@ -14,4 +14,6 @@ for(times = 0; times <= 1; times++){
 function assignNum(){
     randomNum = (Math.floor(Math.random(0,7)*10));
     return randomNum;
-}
+}*/
+
+alert("Does this work?");
